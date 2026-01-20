e64f6baf3744cd1d29d0f0d1d3055ab23dc0a533
Sulav <slvojha7@gmail.com>
2026-01-20 23:30:30 +0545

    Update Navbar casing and frontend assets

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -1,7 +1,9 @@
 const Footer = () => (
     <footer className="text-white text-center flex justify-between py-4 text-sm mt-10 fixed bottom-0 left-0 w-full bg-blue-700/10 z-50 shadow-md">
         <span className="mx-10 text-lg">&copy; 2026 PneumoNet </span>
-        <span className="mx-10 text-lg">Built by Sulav Ojha </span>
+        <span className="mx-10 text-lg font-bold text-red-600">Built by Sulav Ojha</span>
+
+
     </footer>
 );
 
--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -3,13 +3,13 @@ const Navbar = () => {
         <nav className="fixed top-0 left-0 w-full bg-black-700/10 text-white z-50 shadow-md">
             <div className="max-w-8xl mx-auto px-11 py-4 flex justify-between items-center">
                 <h1 className="text-xl sm:text-2xl font-bold tracking-wide">
-                    PneumoAI
+                    Pneumo Net
                 </h1>
                 <div className="space-x-6 hidden md:block">
                     <a href="" className="hover:text-gray-300 font-mona">
                         Documentation
                     </a>
-                    <a href="#" className="hover:text-gray-300 font-mona">
+                    <a href="https://github.com/snatched11/pneumonet" className="hover:text-gray-300 font-mona">
                         Source Code
                     </a>
                     <a

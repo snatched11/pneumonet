0d808b6ff7fe4816df1ff24c99737967890b6ea2
sayyless <123967638+snatched11@users.noreply.github.com>
2026-02-13 14:56:05 +0545

    Update Footer.jsx

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -1,7 +1,7 @@
 const Footer = () => (
     <footer className="text-white text-center flex justify-between py-4 text-sm mt-10 fixed bottom-0 left-0 w-full bg-blue-700/10 z-50 shadow-md">
         <span className="mx-10 text-lg">&copy; 2026 PneumoNet </span>
-        <span className="mx-10 text-lg font-bold text-red-600">Built by Sulav </span>
+        <span className="mx-10 text-lg font-bold text-red-600">Built by Rahul Ojha </span>
 
 
     </footer>
